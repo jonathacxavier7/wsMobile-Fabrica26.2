f7e00d00ac283b302592128b123829df2be02948
Jonatha <juvenilapela@gmail.com>
2026-09-07 20:16:24 -0300

    feat: protege rotas autenticadas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,45 @@
-import { Stack } from "expo-router";
+import { Stack, router, usePathname } from "expo-router";
+import { useEffect, useState } from "react";
+
+import { getToken } from "@/constants/api";
 
 export default function RootLayout() {
+  const pathname = usePathname();
+  const [checkingAuth, setCheckingAuth] = useState(true);
+
+  useEffect(() => {
+    async function protectRoutes() {
+      try {
+        const token = await getToken();
+        const isLogin = pathname === "/";
+
+        if (!token && !isLogin) {
+          router.replace("/");
+          return;
+        }
+
+        if (token && isLogin) {
+          router.replace("/(tabs)/subjects");
+          return;
+        }
+      } finally {
+        setCheckingAuth(false);
+      }
+    }
+
+    protectRoutes();
+  }, [pathname]);
+
+  if (checkingAuth) {
+    return null;
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
+
       <Stack.Screen name="(tabs)" />
+
       <Stack.Screen
         name="subject/[id]"
         options={{
